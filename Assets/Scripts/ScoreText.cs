4ae8942584b90551c4dc27462c4ec786a847d48f
seike1e83cc <107287373+seike1e83cc@users.noreply.github.com>
2022-06-11 11:43:24 +0900

    scoretext

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreText : MonoBehaviour
+{
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
