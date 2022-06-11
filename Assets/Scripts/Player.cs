e2d48be0f4249f7e07c3e60ef44dfe3eed8e3abd
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-06-11 14:01:21 +0900

    プレハブにした

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,15 +7,46 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private PlayerInputManager _input;
+    Vector3 beforePos;
 
+    [SerializeField]
+    LayerMask itoLayer;
+
+    SpriteRenderer spriteRenderer;
     void Start()
     {
         TryGetComponent(out _input);
+        TryGetComponent(out spriteRenderer);
+
+        Cursor.visible = false;
+    }
+    void OnDestroy()
+    {
+        Cursor.visible = true;
     }
 
     void Update()
     {
-        _ = _input.Move.x;
-        _ = _input.Move.y;
+        Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        if (beforePos == touchWorldPosition)
+            return;
+
+        // 位置設定と左右反転
+        var playerPos = touchWorldPosition;
+        playerPos.z = 0;
+        transform.position = playerPos;
+        spriteRenderer.flipX = beforePos.x > touchWorldPosition.x;
+
+
+        var hits = Physics2D.LinecastAll(beforePos, touchWorldPosition, itoLayer);
+
+        foreach (var hit in hits)
+        {
+            hit.transform.TryGetComponent<GameObject>(out var ito);
+            Debug.Log(hit.point);
+        }
+
+        beforePos = touchWorldPosition;
     }
 }
