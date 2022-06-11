d65b729255b34695cd1aea47b8fce767df02dfbc
seike1e83cc <107287373+seike1e83cc@users.noreply.github.com>
2022-06-11 16:49:29 +0900

    visual2
    見た目変更
    ScoreTextスクリプト変更

--- Assets/Scripts/Result/ScoreText.cs
+++ Assets/Scripts/Result/ScoreText.cs
@@ -6,17 +6,29 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class ScoreText : MonoBehaviour
 {
+    public int speed = 10;
     private Text text;
+    private int value = 0;
+    private int target = 0;
 
     void Start()
     {
         text = GetComponent<Text>();
+        target = ScoreManager.Get();
+        ChangeScoreText();
+    }
+
+    void Update()
+    {
+        if (value >= target) return;
+        value += speed;
+        if (value > target) value = target;
         ChangeScoreText();
     }
 
     void ChangeScoreText()
     {
         if (!text) return;
-        text.text = ScoreManager.Get().ToString();
+        text.text = value.ToString();
     }
 }
